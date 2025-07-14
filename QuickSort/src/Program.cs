eb70e0e3b9fb945792de3a1d61e14d30d7543d34
João H. <83355344+joaostack@users.noreply.github.com>
2025-07-14 19:56:54 -0300

    Add files via upload

---
+++ QuickSort/src/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] QuickSort(int[] arr)
+{
+    if (arr.Length <= 1)
+    {
+        return arr;
+    }
+    else
+    {
+        var pivo = arr[arr.Length / 2];
+        var left = arr.Where(x => x < pivo).ToArray();
+        var right = arr.Where(x => x > pivo).ToArray();
+        var middle = arr.Where(x => x == pivo).ToArray();
+
+        return QuickSort(left).Concat(middle).Concat(QuickSort(right)).ToArray();
+    }
+}
+
+int[] arr = [10, 7, 8, 9, 1, 5];
+arr = QuickSort(arr);
+Console.WriteLine($"Sorted: {string.Join(",", arr)}");

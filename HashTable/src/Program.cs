3e2f619093c42370c7f64870bea87939ed64e482
João H. <83355344+joaostack@users.noreply.github.com>
2025-07-15 16:51:41 -0300

    Add files via upload

---
+++ HashTable/src/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+// PEOPLE : VOTED?
+var votes = new Hashtable();
+
+
+void CheckVotes(string name)
+{
+    if (votes.Contains(name))
+    {
+        Console.WriteLine($"Go home, {name}!");
+    }
+    else
+    {
+        votes[name] = true;
+        Console.WriteLine($"{name} voted!");
+    }
+}
+
+CheckVotes("John");
+CheckVotes("Oliver");
+
+// Check vote again
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine(new string('-', 15) + " Checking votes... " + new string('-', 15));
+Console.ResetColor();
+
+CheckVotes("John");
+CheckVotes("Oliver");

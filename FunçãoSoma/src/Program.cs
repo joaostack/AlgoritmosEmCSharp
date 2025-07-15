bd62814d20c6c9ae2c7ed3b9a8d953a1463233e1
João H. <83355344+joaostack@users.noreply.github.com>
2025-07-15 17:05:11 -0300

    Add files via upload

---
+++ FunçãoSoma/src/Program.cs
@@ -0,0 +1,16 @@
+﻿int Soma(List<int> lista)
+{
+    var total = 0;
+
+    foreach (var item in lista)
+    {
+        Console.WriteLine($"{total} + {item}");
+        total += item;
+    }
+
+    return total;
+}
+
+var lista = new List<int>() { 1, 5, 30, 48, 12, 245, 32, 67 };
+var total = Soma(lista);
+Console.WriteLine($"Total: {total}");

b13966e80a5ebfde4b4cff25e9345f4eadd05a36
João H. <83355344+joaostack@users.noreply.github.com>
2025-07-15 17:04:43 -0300

    Add files via upload

---
+++ TotalListItem/src/Program.cs
@@ -0,0 +1,15 @@
+﻿int Count(List<int> lista)
+{
+    if (lista.Count <= 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1 + Count(lista[1..]);
+    }
+}
+
+var lista = new List<int>() { 1, 3, 5, 28, 32, 2, 4, 45, 75 };
+var n = Count(lista);
+Console.WriteLine(n);

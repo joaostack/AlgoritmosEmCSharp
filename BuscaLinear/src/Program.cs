265966f5672f2e93105fa2cabc522a8e8712acc0
João H. <83355344+joaostack@users.noreply.github.com>
2025-07-25 08:17:09 -0300

    Add files via upload

---
+++ BuscaLinear/src/Program.cs
@@ -0,0 +1,17 @@
+﻿int LinearSearch(int[] arr, int target)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == target)
+        {
+            return i; // Return the index of the target element
+        }
+    }
+
+    return -1;
+}
+
+int[] numbers = { 10, 20, 30, 40, 50 };
+int target = 30;
+int index = LinearSearch(numbers, target);
+Console.WriteLine(index != -1 ? $"Element {target} found at index: {index}" : "Element not found");

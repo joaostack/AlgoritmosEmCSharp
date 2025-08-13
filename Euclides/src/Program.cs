405479dbbbb3f1d9f3b42e3b6281cfc6ac3e9808
João H. <83355344+joaostack@users.noreply.github.com>
2025-08-13 19:37:00 -0300

    Add files via upload

---
+++ Euclides/src/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Euclides;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("N1 = ");
+        var n1 = int.Parse(Console.ReadLine());
+
+        Console.Write("N2 = ");
+        var n2 = int.Parse(Console.ReadLine());
+
+        while (n1 != n2)
+        {
+            if (n1 > n2)
+            {
+                n1 = n1 - n2;
+            }
+            else
+            {
+                n2 = n2 - n1;
+            }
+
+            Console.WriteLine($"Current Values => n1={n1} : n2={n2}");
+        }
+
+        Console.WriteLine($"MDC => {n1}");
+    }
+}

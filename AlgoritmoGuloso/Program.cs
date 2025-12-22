dbe27f6ca82c1bbd6e7c0c45294360250a6d62bd
João H. <83355344+joaostack@users.noreply.github.com>
2025-12-22 02:15:30 -0300

    Add files via upload

---
+++ AlgoritmoGuloso/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ProblemaDoTroco
+{
+    public static List<int> ProblemaTroco(int centavos, List<int> trocos) {
+        var total = new List<int>();
+    
+        foreach (var troco in trocos) {
+            while (centavos >= troco) {
+                centavos -= troco;
+                total.Add(troco);
+            }
+        }    
+        
+        return total;
+    }
+    
+    
+    public static void Main(string[] args)
+    {
+        var trocos = new List<int>() { 1, 5, 7 };
+        var resultado = ProblemaTroco(11, trocos);
+        
+        Console.WriteLine($"Resultado = [{string.Join(", ", resultado)}]");
+    }
+}
